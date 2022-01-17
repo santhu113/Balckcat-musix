732fa964d3ce2c6aa6932bb55c522532e1ae3eba
Kanna <91947146+blackcat096@users.noreply.github.com>
2022-01-17 15:27:39 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -62,7 +62,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
 
     image1 = Image.open("./background.png")
-    image2 = Image.open("etc/foreground.png")
+    image2 = Image.open("https://telegra.ph/file/2419b067089193430b11c.jpg")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
