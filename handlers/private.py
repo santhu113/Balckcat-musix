b2f9891534eb2fe927a365e15f8e1cb55629b52e
santhu113 <95135053+santhu113@users.noreply.github.com>
2022-01-21 12:08:11 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,24 +10,24 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/f4e1c2ebc7a14110faf47.jpg",
-        caption=f"""**ʜᴇʟʟᴏ ɪ'ᴍ ᴀᴅᴠᴀɴᴄᴇ ᴍᴜsɪᴄ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ᴠᴘs  ᴄᴀᴛ sᴇᴠᴇ. 
+        photo=f"https://te.legra.ph/file/c981eed742c6acd271ebf.jpg",
+        caption=f"""**ʜᴇʟʟᴏ ɪ'ᴍ ᴀᴅᴠᴀɴᴄᴇ ᴍᴜsɪᴄ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ᴠᴘs  𝐬𝐚𝐧𝐭𝐡𝐮 𝐬𝐞𝐫𝐯𝐞𝐫💞. 
 ┏━━━━━━━━━━━━━━━━━┓
 ┣» ᴏᴘ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. 
 ┣» ʜɪɢʜ ǫᴜᴀʟɪᴛʏ  ᴍᴜꜱɪᴄ.
 ┣» ᴀᴅᴠᴀɴᴄᴇᴅ ꜰᴇᴀᴛᴜʀᴇꜱ.
 ┣» ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ.
 ┗━━━━━━━━━━━━━━━━━┛
-[ʙʟᴀᴄᴋ ᴄᴀᴛ ❤️](https://t.me/catmusicworld)
+[𝐒𝐚𝐧𝐭𝐡𝐮 ❤️](https://t.me/santhu_music_bot)
 
 
 
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [ʙʟᴀᴄᴋ ᴄᴀᴛ🐱❤️](https://t.me/The_cat_lover0)**""",
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐚𝐧𝐭𝐡𝐮 𝐁𝐨𝐭🐱❤️](https://t.me/santhu_music_bot)**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/catmusicworld")
+                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/santhuvc")
                 ]
                 
            ]
@@ -44,7 +44,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💥 ᴄᴏᴍᴍᴀɴᴅs 💞", url=f"https://telegra.ph/%CA%99%CA%9F%E1%B4%80%E1%B4%84%E1%B4%8B-%E1%B4%84%E1%B4%80%E1%B4%9B-01-05")
+                        "💥 𝐁𝐨𝐭'𝐬💞", url=f"https://telegra.ph/Santhosh-01-18")
                 ]
             ]
         ),
