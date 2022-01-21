6fde06be3ebafb70f14f7ff3907e6f7dcfad5bc1
santhu113 <95135053+santhu113@users.noreply.github.com>
2022-01-21 21:13:49 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -11,23 +11,24 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/e719f19bbeeb7f55e6202.jpg",
-        caption=f"""**ʜᴇʟʟᴏ ɪ'ᴍ ᴀᴅᴠᴀɴᴄᴇ ᴍᴜsɪᴄ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ᴠᴘs  𝐬𝐚𝐧𝐭𝐡𝐮 𝐬𝐞𝐫𝐯𝐞𝐫💞. 
+        caption=f"""**ʜᴇʟʟᴏ ɪ'ᴍ ᴀᴅᴠᴀɴᴄᴇ ᴍᴜsɪᴄ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ ᴠᴘs  𝐬𝐚𝐧𝐭𝐡𝐮 sᴇᴠᴇ. 
 ┏━━━━━━━━━━━━━━━━━┓
 ┣» ᴏᴘ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. 
 ┣» ʜɪɢʜ ǫᴜᴀʟɪᴛʏ  ᴍᴜꜱɪᴄ.
 ┣» ᴀᴅᴠᴀɴᴄᴇᴅ ꜰᴇᴀᴛᴜʀᴇꜱ.
-┣» ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ.
+┣» ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ. 
+┣» 𝐃𝐄𝐏𝐋𝐎𝐘 𝐁𝐘 💞[𝚂𝙰𝙽𝚃𝙷𝙾𝚂𝙷 ❤](https://t.me/santhu_music_bot)
 ┗━━━━━━━━━━━━━━━━━┛
-[𝐒𝐚𝐧𝐭𝐡𝐮 ❤️](https://t.me/santhu_music_bot)
+[𝙽𝙴𝚃𝚆𝙾𝚁𝙺 ❤️](https://t.me/santhuvc)
 
 
 
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐚𝐧𝐭𝐡𝐮 𝐁𝐨𝐭🐱❤️](https://t.me/santhu_music_bot)**""",
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐀𝐍𝐓𝐇𝐎𝐒𝐇 ❤️](https://t.me/santhu_music_bot)**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐍𝐀𝐍𝐔 𝐆𝐑𝐎𝐔𝐏 𝐋𝐎 𝐀𝐃𝐃 𝐂𝐇𝐄𝐒𝐔 𝐊𝐎𝐍𝐃𝐈 ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true", 
+                        "🥀 𝐍𝐀𝐍𝐔 𝐀𝐃𝐃 𝐂𝐇𝐄𝐒𝐔 𝐊𝐎𝐍𝐃𝐈 ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true", 
                 ]
                 
            ]
@@ -44,7 +45,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💥 𝐁𝐨𝐭'𝐬💞", url=f"https://telegra.ph/Santhosh-01-18")
+                        "💥 𝙾𝚆𝙽𝙴𝚁 💞", url=f"https://t.me/santhu_music_bot")
                 ]
             ]
         ),
