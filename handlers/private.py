c0449e270ea6a6b3877620958ac718af07e990eb
Santhosh podili <95135053+santhu113@users.noreply.github.com>
2022-01-27 04:59:38 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -27,8 +27,8 @@ async def start_(client: Client, message: Message):
                 [
                     InlineKeyboardButton(
                         "🥀 𝐍𝐀𝐍𝐔 𝐀𝐃𝐃 𝐂𝐇𝐄𝐒𝐔 𝐊𝐎𝐍𝐃𝐈 ✨", url=f"https://t.me/Santhumusicvcplayerbot?startgroup=true")
-                    InlineKeyboardButton(
-                        "ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ​​", callback_data="help")
+                    InlineKeyboardButton("😁 𝐎𝐖𝐍𝐄𝐑 😛​​", url=f"https://t.me/santhu_music_bot")
+                    InlineKeyboardButton("😇 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 😛​​", url=f"https://t.me/santhuvc")
                 ]
                 
            ]
