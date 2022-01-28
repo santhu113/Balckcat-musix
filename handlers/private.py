77fbbc0b991a858770522e5fd768db7eca392083
SANTHOSHPODILIGITHHUB <98577065+SANTHOSHPODILIGITHHUB@users.noreply.github.com>
2022-01-28 23:19:53 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -26,7 +26,7 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💞 ᴀʀʏᴀ ɴɪ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ 😇", url=f"https://t.me/Santhuadvancemusicbot?startgroup
+                        "💞 ᴀʀʏᴀ ɴɪ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ 😇", url=f"https://t.me/Santhuadvancemusicbot?startgroup=true") 
                 ]
                 
            ]
