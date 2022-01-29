ccc5c8175180094a012db714af01594cb8938325
SANTHOSHPODILIGITHHUB <98577065+SANTHOSHPODILIGITHHUB@users.noreply.github.com>
2022-01-29 19:52:37 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,49 +7,56 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
+    await message.reply_sticker("CAACAgQAAxkBAAIC_mH1JUrL_s4kgKA5hiDk_Rrl0GYWAAIeCgACz9YRUXNuChP5kGjfIwQ")
     await message.reply_photo(
-        photo=f"",
-        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [ᴍʀ.sᴀɴᴛʜᴜ❤️](https://t.me/santhu_music_bot)
-┏━━━━━━━━━━━━━━━━━┓
+        photo=f"https://te.legra.ph/file/cdb11db8885e10e042964.jpg",
+        caption=f"""**━━━━━━━━━━━━ 🌺🌻🌹🌷━━━━━━━━━━
+━━━━━━━━━━━━━ 🌺🌻🌹🌷━━━━━━━━━━━━━
+😊ʜɪ ɪᴀᴍ ᴛᴇʟᴇɢʀᴀᴍ ᴠᴏɪᴄᴇ ᴍᴜsɪᴄ ʙᴏᴛ... ᴅᴇᴘʟᴏʏ ʙʏ : @nenu_na_crush
+
+┏━━━━━━━━━━━━━━━━━┓ 🌺🌻🌹🌷🌺🌻🌹
 ┣» ᴏᴘ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. 
 ┣» ʜɪɢʜ ǫᴜᴀʟɪᴛʏ  ᴍᴜꜱɪᴄ.
 ┣» ᴀᴅᴠᴀɴᴄᴇᴅ ꜰᴇᴀᴛᴜʀᴇꜱ.
 ┣» ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ. 
-┣» [𝐃𝐄𝐏𝐋𝐎𝐘 𝐁𝐘 ❤️](https://t.me/santhu_music_bot)
+┣» [𝐃𝐄𝐏𝐋𝐎𝐘 𝐁𝐘 ❤️](https://t.me/nenu_na_crush)
 ┗━━━━━━━━━━━━━━━━━┛
-[𝐎𝐖𝐍𝐄𝐑 ❤️](https://t.me/santhu_music_bot)
-
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐀𝐍𝐓𝐇𝐔❤️](https://t.me/santhu_music_bot)**""",
+[𝐎𝐖𝐍𝐄𝐑 ❤️](https://t.me/nenu_na_crush)
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐀𝐍𝐓𝐇𝐔❤️](https://t.me/nenu_na_crush)
+━━━━━━━━━━━━━━━━━━━━━━**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💞 ᴀʀʏᴀ ɴɪ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ 😇", url=f"https://t.me/iconstararyamusicadvancebot?startgroup=true")
+                        "💞ᴀʀʏᴀ ɴɪ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ💞", url="https://t.me/iconstararyamusicadvancebot?startgroup=true")
+                  ],[
                     InlineKeyboardButton(
-                        "❣️ ᴏᴡɴᴇʀ ɢᴀᴅᴜ 😝", url=f"https://t.me/santhu_music_bot")
+                        "☹️ᴏᴡɴᴇʀ😘", url="https://t.me/@nenu_na_crush"
+                    ),
                     InlineKeyboardButton(
-                        "❣️ ɴᴇᴛᴡᴏʀᴋ 🤩", url=f"https://t.me/santhuvc")
+                        "😇ɢʀᴏᴜᴘ💞", url="https://t.me/friends_life_forever"
+                    )
+                ],[ 
                     InlineKeyboardButton(
-                        "💞 ᴜᴘᴅᴀᴛᴇs 💕", url=f"https://t.me/santhubotupadates")
-                ]
-                
-           ]
-        ),
+                        "😁ɴᴇᴛᴡᴏʀᴋ😊", url="https://t.me/friends_life_forever"
+                    )]
+            ]
+       ),
     )
-    
 
-@Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["repo"]) & filters.group & ~filters.edited & ~filters.private)
+
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/e719f19bbeeb7f55e6202.jpg",
-        caption=f"""""",
+        photo=f"https://te.legra.ph/file/cdb11db8885e10e042964.jpg",
+        caption=f"""ᴘʀɪᴠᴀᴛᴇ ʀᴇᴘᴏ !🖤""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 𝙾𝚆𝙽𝙴𝚁 💞", url=f"https://t.me/santhu_music_bot")
+                        "💕ᴏᴡɴᴇʀ💕", url=f"https://t.me/nenu_na_crush")
                 ]
             ]
         ),
